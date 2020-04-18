ec951ecf2af3d608175b3825cfdf246ea59b22c0
Saibot101 <tobiasmaas98@web.de>
2020-04-18 10:39:11 +0200

    Part 3a finished Part1 + Part2

--- backend/index.js
+++ backend/index.js
@@ -1,81 +1,96 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
 app.use(express.json());
+morgan.token('request', function (req) {
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request'))
 
-let notes = [
+let persons=  [
     {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
     },
     {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
     },
     {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
     }
 ];
 
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-});
+app.get('/api/info', (req, res) => {
+    const send = new Date().toUTCString();
+    const output = 'Phonebook has info for '+persons.length+' people <br/>' + send;
+    console.log(new Date().getUTCDate())
+    res.send(
+        output
+    )
+})
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-});
-
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
-    if(note){
-        response.json(note)
+app.get('/api/persons/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if(person){
+        res.json(person)
     } else {
-        response.status(404).end()
+        res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req,res)=> {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
+const randomNumber = () => {
+    const number = Math.random() * 1000;
 
-    response.status(204).end()
-});
+    if(persons.find(person => person.id === number)){
+        randomNumber();
+    }
+    return Math.floor(number);
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
+
 }
 
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-    if(!body.content){
-        return response.status(400).json({
-            error: 'content missing'
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    if (!body.name || !body.number){
+        return res.status(400).json({
+            error: 'name or number is missing'
         })
     }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId()
+    if(persons.find(person => person.name === body.name)){
+        return res.status(400).json({
+            error: 'name is already in the phonebook'
+        })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: randomNumber()
     }
 
-    notes = notes.concat(note);
+    persons = persons.concat(person)
+    res.status(200).json(person)
+})
 
-    response.json(note)
-});
+
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
